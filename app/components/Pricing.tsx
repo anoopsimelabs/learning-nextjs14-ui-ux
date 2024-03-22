75737ab408c7eea132776bef53782e5832ca6ecc
Anoop Kumar <anoop.kumar@simelabs.com>
2024-03-22 14:14:05 +0530

    Finished learning and creating website

---
+++ app/components/Pricing.tsx
@@ -0,0 +1,149 @@
+import Image from "next/image";
+import Check from "../../public/assets/check.svg";
+
+export function Pricing() {
+  return (
+    <div className="py-[48px] lg:py-[60px]">
+      <h1 className="text-[#172026] text-center font-medium text-2xl lg:text-[42px]">
+        Flexible plans for you
+      </h1>
+      <p className="pt-[16px] pb-[40px] text-center text-[#36485C] lg:text-[18px]">
+        No hidden fees!
+      </p>
+
+      <div className="flex flex-col gap-y-6 lg:flex-row gap-x-6">
+        <div className="w-full rounded-[8px] bg-[#F5F4FF] flex flex-col p-6 lg:justify-between">
+          <div>
+            <h3 className="font-medium text-[#4328EB] text-[18px] lg:text-xl">
+              Free Trial
+            </h3>
+            <p className="pt-[12px] text-[#36485C] lg:text-[18px]">
+              Perfect for testing the waters
+            </p>
+
+            <h2 className="pt-4 text-2xl font-medium lg:text-[32px]">
+              $0<span className="text-[#5F7896]">/mo</span>
+            </h2>
+
+            <ul className="flex flex-col pt-4 gap-y-2 text-[#5F7896]">
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                Lorem ipsum dolor sit amet
+              </li>
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                consectetur adipisicing elit.
+              </li>
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                Neque dolor voluptates
+              </li>
+            </ul>
+          </div>
+
+          <button className="mt-[16px] rounded-[4px] bg-white py-[14px] text-[#4328EB] font-medium">
+            Start Trial
+          </button>
+        </div>
+
+        <div className="w-full rounded-[8px] bg-[#4328EB] flex flex-col p-6">
+          <div>
+            <h3 className="font-medium text-white text-[18px] lg:text-xl">
+              Free Trial
+            </h3>
+            <p className="pt-[12px] text-[#F4F8FA] lg:text-[18px]">
+              Perfect for testing the waters
+            </p>
+
+            <h2 className="pt-4 text-2xl font-medium text-white lg:text-[32px]">
+              $0<span className="text-[#F4F8FA]">/mo</span>
+            </h2>
+
+            <ul className="flex flex-col pt-4 gap-y-2 text-[#F4F8FA]">
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                Lorem ipsum dolor sit amet
+              </li>
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                consectetur adipisicing elit.
+              </li>
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                Neque dolor voluptates
+              </li>
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                Neque dolor voluptates
+              </li>
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                Neque dolor voluptates
+              </li>
+            </ul>
+          </div>
+
+          <button className="mt-[16px] rounded-[4px] bg-white py-[14px] text-[#4328EB] font-medium">
+            Start Trial
+          </button>
+        </div>
+
+        <div className="w-full rounded-[8px] bg-[#F5F4FF] flex flex-col p-6 lg:justify-between">
+          <div>
+            <h3 className="font-medium text-[#4328EB] text-[18px] lg:text-xl">
+              Free Trial
+            </h3>
+            <p className="pt-[12px] text-[#36485C] lg:text-[18px]">
+              Perfect for testing the waters
+            </p>
+
+            <h2 className="pt-4 text-2xl font-medium lg:text-[32px]">
+              $0<span className="text-[#5F7896]">/mo</span>
+            </h2>
+
+            <ul className="flex flex-col pt-4 gap-y-2 text-[#5F7896]">
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                Lorem ipsum dolor sit amet
+              </li>
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                consectetur adipisicing elit.
+              </li>
+              <li className="flex items-center gap-x-2">
+                <span>
+                  <Image src={Check} alt="check" />
+                </span>
+                Neque dolor voluptates
+              </li>
+            </ul>
+          </div>
+
+          <button className="mt-[16px] rounded-[4px] bg-white py-[14px] text-[#4328EB] font-medium">
+            Start Trial
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
